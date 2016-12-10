38eca1b608f2125848cfe5a9932fb20b08095a2e
Vlad <Irbis1307@yandex.ru>
2016-12-10 20:49:12 +0300

    add validation and remove kendo UI

--- Lab_6/App_Start/BundleConfig.cs
+++ Lab_6/App_Start/BundleConfig.cs
@@ -26,6 +26,36 @@ namespace Lab_6
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            bundles.Add(new ScriptBundle("~/bundles/kendo").Include(
+                "~/Scripts/kendo/2016.3.1118/kendo.core.min.js",
+                "~/Scripts/kendo/2016.3.1118/kendo.datepicker.min.js",
+                "~/Scripts/kendo/2016.3.1118/kendo.timepicker.min.js",
+                "~/Scripts/Shared/Layout.js"
+                //"~/Scripts/kendo/2016.3.1118/kendo.all.min.js",
+                //"~/Scripts/kendo/2016.3.1118/kendo.aspnetmvc.min.js"
+                ));
+
+            bundles.Add(new StyleBundle("~/Content/kendo/css").Include(
+                "~/Content/kendo/2016.3.1118/kendo.common-bootstrap.min.css",
+                "~/Content/kendo/2016.3.1118/kendo.bootstrap.min.css"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
+            "~/Scripts/jquery-ui-{version}.js"));
+
+            bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
+              "~/Content/themes/base/jquery.ui.core.css",
+              "~/Content/themes/base/jquery.ui.resizable.css",
+              "~/Content/themes/base/jquery.ui.selectable.css",
+              "~/Content/themes/base/jquery.ui.accordion.css",
+              "~/Content/themes/base/jquery.ui.autocomplete.css",
+              "~/Content/themes/base/jquery.ui.button.css",
+              "~/Content/themes/base/jquery.ui.dialog.css",
+              "~/Content/themes/base/jquery.ui.slider.css",
+              "~/Content/themes/base/jquery.ui.tabs.css",
+              "~/Content/themes/base/jquery.ui.datepicker.css",
+              "~/Content/themes/base/jquery.ui.progressbar.css",
+              "~/Content/themes/base/jquery.ui.theme.css"));
         }
     }
 }
